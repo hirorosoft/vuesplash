af919d4ab6ab0157579310785addbb095a2be349
Hiroshi.Nakai <hrsi.ad@gmail.com>
2019-11-19 13:08:10 +0900

    Vue + Vue Router + Vuex + Laravelで写真共有アプリを作ろう (3) 途中

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 画面遷移はフロントエンドのVueRouterが制御する
+Route::get('/{any?}', function () {
+    return view('index');
+})->where('any', '.+');
